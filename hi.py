369ce623b3e764f13858efd9e7d48c21bc0deca2
ZeroHourStrike <yosefgetachew258@gmail.com>
2024-07-30 10:16:05 +0300

    Add files via upload

---
+++ hi.py
@@ -0,0 +1,12 @@
+a = int(input("right the first number to calculate"))
+b = input("operation to calculate")
+c = int(input("right the last nuber to calculate"))
+
+if b=="+":
+    print(a+c)
+if b=="-":
+    print(a-c)
+if b=="*":
+    print(a*c)
+if b=="/":
+    print(a/c)
